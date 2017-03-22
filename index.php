e9ff4fc1f6960d41fac822dd35308478e01b0c9c
SubN3o <jeremie.daviaud@sfr.fr>
2017-03-22 18:21:01 +0100

    nav travailler + img importer + jquery importer

--- index.php
+++ index.php
@@ -4,15 +4,15 @@
     <meta charset="utf-8">
     <title>Les Jardins de Gaston</title>
     <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="stylesheet.css">
+    <link rel="stylesheet"  href="css/stylesheet.css">
+    <link href="https://fonts.googleapis.com/css?family=Chicle" rel="stylesheet">
 </head>
 <body>
-
 <!-----      Navigation   ------->
 
 <!-- navbar-collapse menu burger-->
-<header>
-    <nav class="navbar navbar-default navbar-fixed-top">
+<header">
+    <nav class="navbar navbar-inverse navbar-fixed-top">
         <div class="container-fluid">
             <div class="navbar-header">
                 <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
@@ -22,17 +22,17 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="#"><img id="logo" src="Images/logo.png"/></a>
-
-
+                <div class="row hidden-xs"><img hidden-xs hidden-sm id="logo" src="img/logo.png"/></div>
+                <div class="row hidden-sm hidden-md hidden-lg"><span class="headtitle">Les Jardins de Gaston</span></div>
             </div>
-            <span class="headtitle">Les Jardins de Gaston</span>
+
             <!-- navbar-collapse navigation -->
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
-                    <li><a href="#">Nav1</a></li>
-                    <li><a href="#">Nav2</a></li>
-                    <li><a href="#">Nav3</a></li>
+                    <li><a href="#">Les prestations</a></li>
+                    <li><a href="#">Le conseil du mois</a></li>
+                    <li><a class="btn btn-success" href="#" role="button">Nous contacter</a></li>
+
                 </ul>
             </div><!-- /.navbar-collapse -->
         </div><!-- /.container-fluid -->
@@ -43,7 +43,7 @@
 <!--------    Image header    --------->
 <div class="row">
     <div class="col-xs-12">
-
+        <img src="img/header.jpg">
     </div>
 </div>
 
@@ -112,6 +112,6 @@
 
 
 </body>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+<script src="js/jquery-3.1.1.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
 </html>
