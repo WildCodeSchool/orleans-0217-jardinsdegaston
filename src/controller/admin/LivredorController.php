dde1c1444208381e879cf8fc3f6bfac631a2fc27
Slix1 <sylvain.jan2@gmail.com>
2017-04-05 17:38:21 +0200

    update form Ldor

--- src/controller/admin/LivredorController.php
+++ src/controller/admin/LivredorController.php
@@ -8,7 +8,9 @@
 
 namespace wcs\controller\admin;
 use wcs\controller\Controller;
-
+use \wcs\model\Livredor;
+use wcs\Form\LivredorForm;
+use wcs\model\DB;
 
 
 class LivredorController extends Controller
@@ -16,26 +18,49 @@ class LivredorController extends Controller
     public function index()
     {
         // --- provisoire ---
-        $test = [   '0' =>
-            [   'Nom' => 'Mr.pouet',
-                'Texte' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor'],
-            '1' =>
-                [   'Nom' => 'Mme.pouet',
-                    'Texte' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor'],
-            '2' =>
-                [   'Nom' => 'Mr.pouet',
-                    'Texte' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor'],
-            '3' =>
-                [   'Nom' => 'Mme.pouet',
-                    'Texte' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor'],
-            '4' =>
-                [   'Nom' => 'Mr.pouet',
-                    'Texte' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor'],
-            '5' =>
-                [   'Nom' => 'Mr.pouet',
-                    'Texte' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor']
-        ];
+//        $test = [   '0' =>
+//            [   'Nom' => 'Mr.pouet',
+//                'Texte' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor'],
+//            '1' =>
+//                [   'Nom' => 'Mme.pouet',
+//                    'Texte' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor'],
+//            '2' =>
+//                [   'Nom' => 'Mr.pouet',
+//                    'Texte' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor'],
+//            '3' =>
+//                [   'Nom' => 'Mme.pouet',
+//                    'Texte' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor'],
+//            '4' =>
+//                [   'Nom' => 'Mr.pouet',
+//                    'Texte' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor'],
+//            '5' =>
+//                [   'Nom' => 'Mr.pouet',
+//                    'Texte' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor']
+//        ];
 
-        return $this->twig->render('Livredor.twig', ['post' => $this->post, 'test' => $test]);
+        $form = new LivredorForm();
+        $Ldor = new Livredor();
+        $param = ["content" => $Ldor->getLdor(),
+                   'form'=>$form];
+
+        return $this->twig->render('Livredor.twig', $param);
+    }
+
+    public function addLdor()
+    {
+//        if (isset($_POST)) {
+//            foreach ($_POST as $key => $val) {
+//                $postClean[$key] = htmlentities(trim($val));
+//            }
+//        }
+        if (isset($post)) {
+            $db = new DB();
+            $query = "INSERT INTO livredor (nom, contenu) VALUES (:NomLDor, :TexteLDor)";
+            $prep = $db->prepare($query);
+            $prep->bindValue(':NomLDor', $post['NomLDor'], \PDO::PARAM_STR);
+            $prep->bindValue(':TexteLDor', $post['TexteLDor'], \PDO::PARAM_STR);
+            $prep->execute();
+            }
+        return $this->index();
     }
 }
